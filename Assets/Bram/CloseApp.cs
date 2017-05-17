c801e660126ed92b2d483cf500aa660d3be130a5
Sgt-Bakker <bram_bakker@live.nl>
2017-05-17 12:26:43 +0200

    Menu
    Menu toegevoegd

---
+++ Assets/Bram/CloseApp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseApp : MonoBehaviour {
+
+	// Use this for initialization
+	public void CloseApplication() {
+        Application.Quit();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
