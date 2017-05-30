cfa29a93b98d39324b583141a18238e8783e27b3
Sgt-Bakker <bram_bakker@live.nl>
2017-05-30 15:41:25 +0200

    Mode toegevoegd aan EditorUI

---
+++ Assets/Bram/Scripts/HideMenus.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideMenus : MonoBehaviour
+{
+    private Canvas Menu;
+
+    void Start()
+    {
+        Menu = GetComponent<Canvas>();
+        Menu.enabled = false;
+    }
+}
---
+++ Assets/Bram/Scripts/ModePicker.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts.VRScenario;
+using Assets.Scripts.Saving;
+using Assets.Scripts;
+
+public class ModePicker : MonoBehaviour
+{
+
+    private Scenario CurrentScenario;
+    public List<GameObject> Prefabs;
+    public EditorCursor CursorPrefab;
+    private EditorCursor EditorCursor;
+    private Dictionary<string, EditorMode> Modes;
+    private EditorMode CurrentMode;
+
+    // Use this for initialization
+    void Start()
+    {
+        EditorCursor = Instantiate(CursorPrefab);
+
+        Modes = new Dictionary<string, EditorMode>();
+        Modes.Add("Cursor", new EditorCursorMode(EditorCursor));
+        Modes.Add("Move", new MoveMode(Camera.main));
+
+        ChangeEditorMode("Cursor");    
+    }
+
+    // Update is called once per frame
+    
+    public void CursorMode()
+    {
+        if (!(CurrentMode is MoveMode))
+        {
+            ChangeEditorMode("Cursor");
+        }
+    }
+    public void MoveMode()
+    {
+        if (!(CurrentMode is EditorCursorMode))
+        {
+            ChangeEditorMode("Move");
+        }
+        
+
+    CurrentMode.Update();
+}
+
+  
+    private void ChangeEditorMode(string modeName)
+    {
+        if (CurrentMode != null)
+        {
+            CurrentMode.Disable();
+        }
+
+        CurrentMode = Modes[modeName];
+
+        CurrentMode.Enable();
+    }
+}
