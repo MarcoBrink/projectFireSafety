89765f77f31af7c966f7ce1f969c0519ffadf9af
Sgt-Bakker <bram_bakker@live.nl>
2017-06-09 11:32:38 +0200

    Editor + Saves UI

---
+++ Assets/Bram/Scripts/GetSaves.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEngine;
+
+public class GetSaves : MonoBehaviour {
+
+    public static string[] GetSavedScenarios()
+    {
+       // string[] foundFiles = Directory.GetFiles(SaveDirectory);
+        List<string> validFiles = new List<string>();
+
+      //  foreach (string path in foundFiles)
+        {
+        //    if (Path.GetExtension(path) == ".pvrs")
+            {
+      //          string filename = Path.GetFileNameWithoutExtension(path);
+        //        validFiles.Add(filename);
+            }
+        }
+
+        return validFiles.ToArray();
+    }
+}
---
+++ Assets/Bram/Scripts/ToScenarios.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToScenarios : MonoBehaviour
+{
+    //Code om de Scene Editor te openen en deze Scene te sluiten
+    public void OpenScene()
+    {
+        SceneManager.LoadScene("Scenarios", LoadSceneMode.Single);
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("Scenarios"));
+    }
+}
