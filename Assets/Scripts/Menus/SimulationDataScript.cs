cc0f1e9f7e4b57d99485a375cc225a5cdb93379f
JannickZeegers <JannickZeegers@gmail.com>
2017-06-20 12:16:47 +0200

    Added HostOrClient Scene + some scripts.

---
+++ Assets/Scripts/Menus/SimulationDataScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulationDataScript : MonoBehaviour {
+
+    public string IP;
+    public bool IsHost;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+}
